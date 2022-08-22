81d68f6e189b2b2302af787ddf553fa1ead7f8a0
Fred Ma <fredma1234@gmail.com>
2022-08-22 18:59:08 -0400

    Added application.properties with bank.slogan property and modified Transaction and TransactionService accordingly to add the bank slogan to every Transaction

--- mod2-ex-mybank/src/main/scala/com/marcobehler/context/BankApplicationConfiguration.scala
+++ mod2-ex-mybank/src/main/scala/com/marcobehler/context/BankApplicationConfiguration.scala
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.marcobehler.BankApplicationLauncher2
-import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
+import org.springframework.context.annotation.{Bean, ComponentScan, Configuration, PropertySource}
 
 @Configuration
 @ComponentScan(basePackageClasses = Array(classOf[BankApplicationLauncher2]))
+@PropertySource(value = Array("classpath:/application.properties"))
 class BankApplicationConfiguration {
   @Bean
   def objectMapper(): ObjectMapper = new ObjectMapper()
--- mod2-ex-mybank/src/main/scala/com/marcobehler/service/TransactionService.scala
+++ mod2-ex-mybank/src/main/scala/com/marcobehler/service/TransactionService.scala
@@ -1,13 +1,14 @@
 package com.marcobehler.service
 
 import com.marcobehler.model.Transaction
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 import java.util.concurrent.CopyOnWriteArrayList
 import java.util.{List => JList}
 
 @Component
-class TransactionService {
+class TransactionService(@Value("${bank.slogan}") bankSlogan: String) {
   private val transactions = new CopyOnWriteArrayList[Transaction]()
 
   def findAll(): JList[Transaction] = {
@@ -15,7 +16,7 @@ class TransactionService {
   }
 
   def create(amount: BigDecimal, reference: String): Transaction = {
-    val transaction = Transaction(amount, reference)
+    val transaction = Transaction(amount, reference, bankSlogan)
     transactions.add(transaction)
     transaction
   }

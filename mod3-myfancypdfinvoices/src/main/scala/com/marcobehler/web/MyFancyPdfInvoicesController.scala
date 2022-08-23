22b0a0c2b868603dbf91fe264d5663447f302201
Fred Ma <fredma1234@gmail.com>
2022-08-23 17:21:41 -0400

    Added POST method using Path Variables in MyFancyPdfInvoicesController

--- mod3-myfancypdfinvoices/src/main/scala/com/marcobehler/web/MyFancyPdfInvoicesController.scala
+++ mod3-myfancypdfinvoices/src/main/scala/com/marcobehler/web/MyFancyPdfInvoicesController.scala
@@ -2,7 +2,7 @@ package com.marcobehler.web
 
 import com.marcobehler.model.Invoice
 import com.marcobehler.service.InvoiceService
-import org.springframework.web.bind.annotation.{GetMapping, PostMapping, RequestParam, ResponseBody, RestController}
+import org.springframework.web.bind.annotation.{GetMapping, PathVariable, PostMapping, RequestParam, ResponseBody, RestController}
 
 import java.util.{List => JList}
 
@@ -16,6 +16,11 @@ class MyFancyPdfInvoicesController(invoiceService: InvoiceService) {
 
   @PostMapping(Array("/invoices"))
   def createInvoice(@RequestParam("user_id") userId: String, @RequestParam amount: Int): Invoice = {
+    invoiceService.create(userId, amount)
+  }
+
+  @PostMapping(Array("/invoices/{userId}/{amount}"))
+  def createInvoicePathVariable(@PathVariable userId: String, @PathVariable amount: Int): Invoice = {
     invoiceService.create(userId, amount)
   }
 }

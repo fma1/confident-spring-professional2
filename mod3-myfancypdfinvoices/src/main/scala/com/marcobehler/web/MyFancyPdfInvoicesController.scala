14bd0e08ebdb623fe7c43ea7ecaf06b1228006a1
Fred Ma <fredma1234@gmail.com>
2022-08-23 17:19:00 -0400

    Added POST functionality to MyFancyPdfInvoicesController

--- mod3-myfancypdfinvoices/src/main/scala/com/marcobehler/web/MyFancyPdfInvoicesController.scala
+++ mod3-myfancypdfinvoices/src/main/scala/com/marcobehler/web/MyFancyPdfInvoicesController.scala
@@ -2,7 +2,7 @@ package com.marcobehler.web
 
 import com.marcobehler.model.Invoice
 import com.marcobehler.service.InvoiceService
-import org.springframework.web.bind.annotation.{GetMapping, ResponseBody, RestController}
+import org.springframework.web.bind.annotation.{GetMapping, PostMapping, RequestParam, ResponseBody, RestController}
 
 import java.util.{List => JList}
 
@@ -12,5 +12,10 @@ class MyFancyPdfInvoicesController(invoiceService: InvoiceService) {
   @GetMapping(Array("/invoices"))
   def invoices(): JList[Invoice] = {
     invoiceService.findAll()
+  }
+
+  @PostMapping(Array("/invoices"))
+  def createInvoice(@RequestParam("user_id") userId: String, @RequestParam amount: Int): Invoice = {
+    invoiceService.create(userId, amount)
   }
 }

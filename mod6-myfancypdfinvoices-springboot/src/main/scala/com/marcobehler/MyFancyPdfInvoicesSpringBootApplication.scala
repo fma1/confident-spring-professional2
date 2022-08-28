2514a89b1b00a31bc4d8b881a44ec0e8e0e76207
Fred Ma <fredma1234@gmail.com>
2022-08-28 16:13:13 -0400

    Created mod7-marcobehler-spring-boot-starter Spring AutoConfiguration module and modified mod6-myfancypdfinvoices-springboot to use it for ViburDBCPDataSource instead of Spring's default HikariDataSource

--- mod6-myfancypdfinvoices-springboot/src/main/scala/com/marcobehler/MyFancyPdfInvoicesSpringBootApplication.scala
+++ mod6-myfancypdfinvoices-springboot/src/main/scala/com/marcobehler/MyFancyPdfInvoicesSpringBootApplication.scala
@@ -1,7 +1,11 @@
 package com.marcobehler
 
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+
+import javax.sql.DataSource
 
 object MyFancyPdfInvoicesSpringBootApplication {
   def main(args: Array[String]): Unit = {
@@ -10,4 +14,11 @@ object MyFancyPdfInvoicesSpringBootApplication {
 }
 
 @SpringBootApplication
-class MyFancyPdfInvoicesSpringBootApplication
+class MyFancyPdfInvoicesSpringBootApplication {
+  @Bean
+  def runner(dataSource: DataSource): InitializingBean = {
+    () => {
+      println(s"This is the datasource this Spring Boot project is using: $dataSource");
+    }
+  }
+}
